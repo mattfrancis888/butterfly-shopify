5f20d60dc254629732fdfbe5ba9e87d48c60bf33
Matthew Francis <mattfrancis888@gmail.com>
2021-05-13 19:04:03 -0400

    Added navigation by enabling embedded app.

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,15 @@ import { TextStyle, EmptyState, Layout, Page } from "@shopify/polaris";
 import store from "store-js";
 const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
 import ResourceListWithProducts from "../components/ResourceList";
+import { TitleBar } from "@shopify/app-bridge-react";
 const Index = () => (
     <Page>
+        <TitleBar
+            title="Sample App"
+            primaryAction={{
+                content: "Select products",
+            }}
+        />
         <Layout>
             <TextStyle variation="positive">
                 Sample app using React and Next.js
